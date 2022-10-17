7cd76edab29a5d55077da99635b129e3d2f2bde7
tracer-ysyx2205 <tracer@ysyx.org>
2022-10-17 17:15:37 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 5.19.13-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 04 Oct 2022 14:37:01 +0000 x86_64 GNU/Linux  17:15:37 up  2:41, 10 users,  load average: 2.97, 3.49, 3.60

--- npc/csrc/main.cc
+++ npc/csrc/main.cc
@@ -4,28 +4,26 @@
 #include <verilated.h>
 #include <nvboard.h>
 
-// static TOP_NAME dut;
-//
-// void nvboard_bind_all_pins(Vtop* top);
-//
-// static void single_cycle() {
-//   dut.clk = 0; dut.eval();
-//   dut.clk = 1; dut.eval();
-// }
-//
-// static void reset(int n) {
-//   dut.rst = 1;
-//   while (n -- > 0) single_cycle();
-//   dut.rst = 0;
-// }
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vtop* top);
+
+static void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
+
+static void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0) single_cycle();
+  dut.rst = 0;
+}
 
 int main(int argc, char** argv, char** env) {
 VerilatedContext* contextp = new VerilatedContext;
 contextp->commandArgs(argc, argv);
 Vtop* top = new Vtop{contextp};
-uint64_t a = 64;
-// while (!contextp->gotFinish()) { 
-while (a--) { 
+while (!contextp->gotFinish()) { 
 	int a = rand() & 1;
 	int b = rand() & 1;
 	top->a = a;
