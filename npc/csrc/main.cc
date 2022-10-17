588d5d0539791bf3ecb612e0cbe3f516cdf9cda6
tracer-ysyx2205 <tracer@ysyx.org>
2022-10-17 15:22:47 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 5.19.13-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 04 Oct 2022 14:37:01 +0000 x86_64 GNU/Linux  15:22:47 up 48 min,  9 users,  load average: 4.30, 4.42, 7.56

---
+++ npc/csrc/main.cc
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <assert.h>
+#include "Vour.h"
+#include "verilated.h"
+int main(int argc, char** argv, char** env) {
+VerilatedContext* contextp = new VerilatedContext;
+contextp->commandArgs(argc, argv);
+Vour* top = new Vour{contextp};
+while (!contextp->gotFinish()) { 
+	int a = rand() & 1;
+	int b = rand() & 1;
+	top->a = a;
+	top->b = b;
+	top->eval(); 
+	printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+	assert(top->f == (a ^ b));
+}
+delete top;
+delete contextp;
+return 0;
+}
