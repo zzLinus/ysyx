791ec07a649789e5d0b88a321bd7f2f29f0c6abd
tracer-ysyx2205 <tracer@ysyx.org>
2022-10-18 17:20:50 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 5.19.13-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 04 Oct 2022 14:37:01 +0000 x86_64 GNU/Linux  17:20:50 up 51 min,  8 users,  load average: 3.49, 3.18, 3.06

--- npc/csrc/main.cc
+++ npc/csrc/main.cc
@@ -38,6 +38,7 @@ int main(int argc, char** argv, char** env)
     nvboard_bind_all_pins(top);
     nvboard_init();
     tfp->open("logs/vlt_dump.vcd");
+    tfp->dumpvars(1, "top");
 
     reset(10);
 
