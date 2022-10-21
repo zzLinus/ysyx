c29fb70ecd5898136965e826ef07fa9d202ce0c5
tracer-ysyx2205 <tracer@ysyx.org>
2022-10-21 21:43:21 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 5.19.13-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 04 Oct 2022 14:37:01 +0000 x86_64 GNU/Linux  21:43:21 up  1:58,  7 users,  load average: 2.51, 3.02, 3.18

--- npc/vsrc/multiselector.v
+++ npc/vsrc/multiselector.v
@@ -1,7 +1,7 @@
 module mux41(a,s,y);
-  input  [7:0] a;  // 声明一个wire型输入变量a，其变量宽度是4位的。
-  input  [1:0] s;  // 声明一个wire型输入变量s，其变量宽度是2位的。
-  output reg [1:0] y;   // 声明一个1位reg型的输出变量y。
+  input  [7:0] a;  
+  input  [1:0] s;  
+  output reg [1:0] y;   
 
   always @ (s or a)
     case (s)
