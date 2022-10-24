5f8771c876fda96716f35dcb6095c495460cd43f
tracer-ysyx2205 <tracer@ysyx.org>
2022-10-24 21:15:16 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 5.19.13-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 04 Oct 2022 14:37:01 +0000 x86_64 GNU/Linux  21:15:16 up 53 min,  4 users,  load average: 3.03, 2.82, 2.73

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -6,8 +6,8 @@ module adder (
 	output reg c_out,
 );
 
-reg tmp = (a|b)&~(a&b)
-assign s = (temp|c)&~(temp&c)
-assign c_out = (a&b)|(c&tmp)
+reg tmp = (a|b)&~(a&b);
+assign s = (temp|c)&~(temp&c);
+assign c_out = (a&b)|(c&tmp);
 
 endmodule
