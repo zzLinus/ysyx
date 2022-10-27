81796893749e6a3fd017adb093548af7f6e7edb9
tracer-ysyx2205 <tracer@ysyx.org>
2022-10-27 16:26:05 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 5.19.13-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 04 Oct 2022 14:37:01 +0000 x86_64 GNU/Linux  16:26:05 up  1:25,  8 users,  load average: 2.89, 3.01, 3.02

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -41,7 +41,6 @@ module alu_4bit(
 	output reg alu_carry
 );
 
-reg [3:0] tmp = 4'b0000;
 
 always @(*) begin
 	case(alu_fnselec)
@@ -52,10 +51,10 @@ always @(*) begin
 			assign alu_zero = ~(|alu_res);
 		end
 		3'b001 : begin
-			assign tmp = (~alu_b + 1);
+			
 			assign alu_carry = 1'b0;
-			{ alu_carry,alu_res } = alu_a + tmp;
-			assign alu_overflow = (alu_a[3] == tmp[3]) && (alu_res[3] != alu_a[3]);
+			{ alu_carry,alu_res } = alu_a + (~alu_b + 1);
+			assign alu_overflow = (alu_a[3] == ~alu_b[3]) && (alu_res[3] != alu_a[3]);
 			assign alu_zero = ~(|alu_res);
 		end
 		3'b010 : begin
