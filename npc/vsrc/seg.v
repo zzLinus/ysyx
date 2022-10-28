b9dc1fe3e07f3620a30715f3aa48289535f735b9
tracer-ysyx2205 <tracer@ysyx.org>
2022-10-28 20:02:59 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 5.19.13-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 04 Oct 2022 14:37:01 +0000 x86_64 GNU/Linux  20:02:59 up  6:44, 12 users,  load average: 3.74, 3.71, 3.73

--- npc/vsrc/seg.v
+++ npc/vsrc/seg.v
@@ -1,8 +1,8 @@
 module seg(
   input clk,
   input rst,
-  input [2:0] seg_x,
-  input [2:0] seg_y,
+  input [3:0] seg_x,
+  input [3:0] seg_y,
   output [7:0] o_seg0,
   output [7:0] o_seg1,
   output [7:0] o_seg2,
@@ -13,7 +13,7 @@ module seg(
   output [7:0] o_seg7
 );
 
-wire [7:0] segs [7:0];
+wire [7:0] segs [9:0];
 assign segs[0] = 8'b11111101;
 assign segs[1] = 8'b01100000;
 assign segs[2] = 8'b11011010;
@@ -22,6 +22,8 @@ assign segs[4] = 8'b01100110;
 assign segs[5] = 8'b10110110;
 assign segs[6] = 8'b10111110;
 assign segs[7] = 8'b11100000;
+assign segs[8] = 8'b11111111;
+assign segs[9] = 8'b111001100;
 
 parameter CLK_NUM = 5000000;
 
