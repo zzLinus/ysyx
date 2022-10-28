5fddec0ba3f261c589bf016237124835230a45c6
tracer-ysyx2205 <tracer@ysyx.org>
2022-10-28 20:17:54 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 5.19.13-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 04 Oct 2022 14:37:01 +0000 x86_64 GNU/Linux  20:17:54 up  6:59, 12 users,  load average: 4.17, 3.87, 3.92

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -126,6 +126,8 @@ reg [7:0] seg_x;
 reg [7:0] seg_y;
 
 always @(inc_counter_out) begin
+	if(inc_counter_out == 99)
+		inc_counter_out <= 0;
 	seg_x = inc_counter_out % 10;
 	seg_y = inc_counter_out / 10;
 end
