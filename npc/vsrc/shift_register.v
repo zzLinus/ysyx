696efdb65479c7c1caa45d1aacd304de23474667
tracer-ysyx2205 <tracer@ysyx.org>
2022-10-30 17:16:59 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 5.19.13-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Tue, 04 Oct 2022 14:37:01 +0000 x86_64 GNU/Linux  17:16:59 up  1:18,  6 users,  load average: 2.65, 2.83, 2.89

--- npc/vsrc/shift_register.v
+++ npc/vsrc/shift_register.v
@@ -8,15 +8,10 @@ module shift_register(
 	output [31:0] out_q
 );
 
-reg [4:0] tmp;
-
-initial begin
-	tmp = shamt;
-end
 
 always @(*) begin
 	if(l_or_r == 1) begin // shift left
-		out_q <= {data[31-tmp:0],{(tmp){1'b0}}};
+		out_q <= {data[31-shamt:0],{(shamt){1'b0}}};
 	end
 	else begin // shift right
 		if(a_or_l == 1) begin // algo shift
