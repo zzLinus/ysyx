9770e09ceba641cc1b7be91db25ce60d90eaff04
tracer-ysyx2205 <tracer@ysyx.org>
2022-11-11 16:46:07 +0800

    > compile NEMU ysyx_22050606 张正 Linux zzGaruda 6.0.7-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 03 Nov 2022 18:02:00 +0000 x86_64 GNU/Linux  16:46:07 up  2:46,  7 users,  load average: 2.50, 3.04, 3.20

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -71,7 +71,7 @@ static void execute(uint64_t n) {
   Decode s;
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc);
-    g_nr_guest_inst ++;
+	g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
