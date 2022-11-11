36b1f52febd74400fade5ca398f2f0e83d607c18
tracer-ysyx2205 <tracer@ysyx.org>
2022-11-11 20:34:09 +0800

    > compile NEMU ysyx_22050606 张正 Linux zzGaruda 6.0.7-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 03 Nov 2022 18:02:00 +0000 x86_64 GNU/Linux  20:34:09 up  6:34,  7 users,  load average: 5.32, 4.10, 3.46

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -15,17 +15,25 @@
 
 #include <isa.h>
 #include "local-include/reg.h"
+#define NOTIMPLEMENT 0
 
-const char *regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+// clang-format off
+const char *regs[] = { // cpu_gpr[32]
+	"$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+	"s0", "s1", "a0",  "a1",  "a2", "a3", "a4", "a5",
+	"a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+	"s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
+// clang-format on
 
-void isa_reg_display() {
+void isa_reg_display()
+{
+	for (int i = 0; i < 31; i++) {
+		printf("register name: %s value: %lu \n", reg_name(i, NOTIMPLEMENT), cpu.gpr[i]);
+	}
 }
 
-word_t isa_reg_str2val(const char *s, bool *success) {
-  return 0;
+word_t isa_reg_str2val(const char *s, bool *success)
+{
+	return 0;
 }
--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -14,6 +14,7 @@
 ***************************************************************************************/
 
 #include <isa.h>
+#include <stdlib.h>
 #include <cpu/cpu.h>
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -25,31 +26,33 @@ void init_regex();
 void init_wp_pool();
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
-static char* rl_gets() {
-  static char *line_read = NULL;
+static char *rl_gets()
+{
+	static char *line_read = NULL;
 
-  if (line_read) {
-    free(line_read);
-    line_read = NULL;
-  }
+	if (line_read) {
+		free(line_read);
+		line_read = NULL;
+	}
 
-  line_read = readline("(nemu) ");
+	line_read = readline("(nemu) ");
 
-  if (line_read && *line_read) {
-    add_history(line_read);
-  }
+	if (line_read && *line_read) {
+		add_history(line_read);
+	}
 
-  return line_read;
+	return line_read;
 }
 
-static int cmd_c(char *args) {
-  cpu_exec(-1);
-  return 0;
+static int cmd_c(char *args)
+{
+	cpu_exec(-1);
+	return 0;
 }
 
-
-static int cmd_q(char *args) {
-  return -1;
+static int cmd_q(char *args)
+{
+	return -1;
 }
 
 static int cmd_help(char *args);
@@ -67,20 +70,21 @@ static int cmd_w(char *args);
 static int cmd_d(char *args);
 
 static struct {
-  const char *name;
-  const char *description;
-  int (*handler) (char *);
-} cmd_table [] = {
-  { "help", "Display information about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
-  { "si", "Single step N times", cmd_si },
-  { "info", "Print register value", cmd_info },
-  { "x", "Value EXPR and store it's resalt in hex, print N bytes start with the resalt that's been store", cmd_x },
-  { "p", "Evaluate EXPR", cmd_p },
-  { "w", "Watch EXPR,if it change stop the program", cmd_w },
-  { "d", "Delete watch point number N", cmd_d },
-  /* TODO: Add more commands */
+	const char *name;
+	const char *description;
+	int (*handler)(char *);
+} cmd_table[] = {
+	{ "help", "Display information about all supported commands", cmd_help },
+	{ "c", "Continue the execution of the program", cmd_c },
+	{ "q", "Exit NEMU", cmd_q },
+	{ "si", "Single step N times", cmd_si },
+	{ "info", "Print register value", cmd_info },
+	{ "x", "Value EXPR and store it's resalt in hex, print N bytes start with the resalt that's been store",
+	  cmd_x },
+	{ "p", "Evaluate EXPR", cmd_p },
+	{ "w", "Watch EXPR,if it change stop the program", cmd_w },
+	{ "d", "Delete watch point number N", cmd_d },
+	/* TODO: Add more commands */
 };
 
 static int cmd_si(char *args)
@@ -91,6 +95,12 @@ static int cmd_si(char *args)
 
 static int cmd_info(char *args)
 {
+	if (strcmp(args, "r")) {
+		isa_reg_display();
+	} else if (strcmp(args, "w")) {
+		printf("watch point info display uninplemente now");
+	} else
+		printf("Unavaliable args");
 	return 0;
 }
 
@@ -114,78 +124,86 @@ static int cmd_d(char *args)
 	return 0;
 }
 
-
 #define NR_CMD ARRLEN(cmd_table)
 
-static int cmd_help(char *args) {
-  /* extract the first argument */
-  char *arg = strtok(NULL, " ");
-  int i;
+static int cmd_help(char *args)
+{
+	/* extract the first argument */
+	char *arg = strtok(NULL, " ");
+	int i;
 
-  if (arg == NULL) {
-    /* no argument given */
-    for (i = 0; i < NR_CMD; i ++) {
-      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-    }
-  }
-  else {
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(arg, cmd_table[i].name) == 0) {
-        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-        return 0;
-      }
-    }
-    printf("Unknown command '%s'\n", arg);
-  }
-  return 0;
+	if (arg == NULL) {
+		/* no argument given */
+		for (i = 0; i < NR_CMD; i++) {
+			printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+		}
+	} else {
+		for (i = 0; i < NR_CMD; i++) {
+			if (strcmp(arg, cmd_table[i].name) == 0) {
+				printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+				return 0;
+			}
+		}
+		printf("Unknown command '%s'\n", arg);
+	}
+	return 0;
 }
 
-void sdb_set_batch_mode() {
-  is_batch_mode = true;
+void sdb_set_batch_mode()
+{
+	is_batch_mode = true;
 }
 
-void sdb_mainloop() {
-  if (is_batch_mode) {
-    cmd_c(NULL);
-    return;
-  }
+void sdb_mainloop()
+{
+	if (is_batch_mode) {
+		cmd_c(NULL);
+		return;
+	}
 
-  for (char *str; (str = rl_gets()) != NULL; ) {
-    char *str_end = str + strlen(str);
+	for (char *str; (str = rl_gets()) != NULL;) {
+		char *str_end = str + strlen(str);
 
-    /* extract the first token as the command */
-    char *cmd = strtok(str, " ");
-    if (cmd == NULL) { continue; }
+		/* extract the first token as the command */
+		char *cmd = strtok(str, " ");
+		if (cmd == NULL) {
+			continue;
+		}
 
-    /* treat the remaining string as the arguments,
+		/* treat the remaining string as the arguments,
      * which may need further parsing
      */
-    char *args = cmd + strlen(cmd) + 1;
-    if (args >= str_end) {
-      args = NULL;
-    }
+		char *args = cmd + strlen(cmd) + 1;
+		if (args >= str_end) {
+			args = NULL;
+		}
 
 #ifdef CONFIG_DEVICE
-    extern void sdl_clear_event_queue();
-    sdl_clear_event_queue();
+		extern void sdl_clear_event_queue();
+		sdl_clear_event_queue();
 #endif
 
-    int i;
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { return; }
-        break;
-      }
-    }
+		int i;
+		for (i = 0; i < NR_CMD; i++) {
+			if (strcmp(cmd, cmd_table[i].name) == 0) {
+				if (cmd_table[i].handler(args) < 0) {
+					return;
+				}
+				break;
+			}
+		}
 
-    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
-  }
+		if (i == NR_CMD) {
+			printf("Unknown command '%s'\n", cmd);
+		}
+	}
 }
 
-void init_sdb() {
-  /* Compile the regular expressions. */
-  init_regex();
+void init_sdb()
+{
+	/* Compile the regular expressions. */
+	init_regex();
 
-  /* Initialize the watchpoint pool. */
-  init_wp_pool();
+	/* Initialize the watchpoint pool. */
+	init_wp_pool();
 }
