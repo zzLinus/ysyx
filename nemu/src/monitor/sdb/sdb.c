5ad05eac9f7cbd57bf0b57b6fb4437ffec3bd70d
tracer-ysyx2205 <tracer@ysyx.org>
2022-11-12 21:07:49 +0800

    > compile NEMU ysyx_22050606 张正 Linux zzGaruda 6.0.7-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 03 Nov 2022 18:02:00 +0000 x86_64 GNU/Linux  21:07:49 up 53 min,  2 users,  load average: 2.27, 2.18, 2.26

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -134,22 +134,22 @@ static int cmd_x(char *args)
 		arg1 += 8;
 		printf("0x%016lx\n", paddr_read(arg1, 8));
 	}
-	arg0 /= 8;
+	arg0 %= 8;
 	for (i = 0; i < arg0 / 4; ++i) {
 		arg1 += 4;
 		printf("0x%016lx\n", paddr_read(arg1, 4));
 	}
-	arg0 /= 4;
+	arg0 %= 4;
 	for (i = 0; i < arg0 / 2; ++i) {
 		arg1 += 2;
 		printf("0x%016lx\n", paddr_read(arg1, 2));
 	}
-	arg0 /= 2;
+	arg0 %= 2;
 	for (i = 0; i < arg0 / 1; ++i) {
 		arg1 += 1;
 		printf("0x%016lx\n", paddr_read(arg1, 1));
 	}
-	arg0 /= 2;
+	arg0 %= 2;
 	return 0;
 }
 
