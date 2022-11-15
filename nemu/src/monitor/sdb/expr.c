11402eb5c98b2c8832d8c4ccb33b0acdf7514cb5
tracer-ysyx2205 <tracer@ysyx.org>
2022-11-15 16:20:14 +0800

    > compile NEMU ysyx_22050606 张正 Linux zzGaruda 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  16:20:14 up  2:12,  6 users,  load average: 3.12, 3.26, 3.23

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -48,7 +48,7 @@ static struct rule {
 	{ "\\)", ')' }, // right breck
 	{ "==", TK_EQ }, // equal
 	{ "[0-9]+", TK_NUM }, // number
-	{ "$[a-zA-Z]+", TK_REG }, // number
+	{ "$[a-zA-Z0-9]+", TK_REG }, // number
 	{ "0[xX][0-9a-fA-F]+", TK_HEX }, // number
 };
 
