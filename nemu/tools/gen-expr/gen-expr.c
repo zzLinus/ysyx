a34453f7809ea0c771cbbb1ee79fe28a1881d26a
tracer-ysyx2205 <tracer@ysyx.org>
2022-11-15 15:07:40 +0800

    > compile NEMU ysyx_22050606 张正 Linux zzGaruda 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  15:07:40 up  1:00,  6 users,  load average: 3.00, 3.23, 3.14

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -19,9 +19,10 @@
 #include <time.h>
 #include <assert.h>
 #include <string.h>
+#include <stdlib.h>
 
 // this should be enough
-static char buf[65536] = {};
+static char buf[65536] = { 0 };
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format = "#include <stdio.h>\n"
 			   "int main() { "
@@ -33,12 +34,13 @@ static char *code_format = "#include <stdio.h>\n"
 uint32_t choose(uint32_t n);
 void gen(char c);
 void gen_rand_op(void);
+void gen_num(uint32_t n);
 
 static void gen_rand_expr()
 {
 	switch (choose(3)) {
 	case 0:
-		choose(9);
+		gen_num(10);
 		break;
 	case 1:
 		gen('(');
@@ -53,17 +55,42 @@ static void gen_rand_expr()
 	}
 }
 
-uint32_t choose(uint32_t n)
-{
-	return 0;
-}
-
 void gen(char c)
 {
+	char str[2] = "\0";
+	str[0] = c;
+	strcat(buf, str);
 }
 
 void gen_rand_op(void)
 {
+	switch (choose(4)) {
+	case 0:
+		strcat(buf, "-");
+		break;
+	case 1:
+		strcat(buf, "+");
+		break;
+	case 2:
+		strcat(buf, "*");
+		break;
+	case 3:
+		strcat(buf, "/");
+		break;
+	}
+}
+
+void gen_num(uint32_t n)
+{
+	int num = choose(n);
+	char str[8];
+	snprintf(str, 8, "%d", num);
+	strcat(buf, str);
+}
+
+uint32_t choose(uint32_t n)
+{
+	return rand() % n;
 }
 
 int main(int argc, char *argv[])
