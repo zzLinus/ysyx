f258d93d49e2392623abb280e63fd97ce6373aaf
tracer-ysyx2205 <tracer@ysyx.org>
2022-11-15 16:46:33 +0800

    > compile NEMU ysyx_22050606 张正 Linux zzGaruda 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  16:46:33 up  2:38,  6 users,  load average: 3.25, 3.30, 3.27

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -143,6 +143,7 @@ static bool make_token(char *e)
 					break;
 				case TK_REG:
 					tokens[nr_token].type = TK_REG;
+					// remove the '$' sign
 					strncpy(tokens[nr_token++].str, substr_start + 1, substr_len - 1);
 					break;
 				case TK_HEX:
