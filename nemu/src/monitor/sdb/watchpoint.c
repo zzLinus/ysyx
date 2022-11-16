78964a553c08235988f88d1683a4af94836a0983
tracer-ysyx2205 <tracer@ysyx.org>
2022-11-16 17:19:40 +0800

    > compile NEMU ysyx_22050606 张正 Linux zzGaruda 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  17:19:40 up  2:04,  3 users,  load average: 1.80, 1.91, 1.96

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -52,7 +52,7 @@ void create_wp(char *args, bool *success)
 {
 	WP *wp = new_wp();
 	printf("%s\n", args);
-	memmove(args, args + 1, strlen(args) - 1);
+	memmove(args, args + 1, strlen(args));
 	printf("%s\n", args);
 	strcpy(wp->var_name, args);
 	wp->value = eval_reg(args);
