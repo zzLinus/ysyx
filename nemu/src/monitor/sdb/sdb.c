af5e45799b4102721d3093a934c2e6c3714fd55e
tracer-ysyx2205 <tracer@ysyx.org>
2022-11-17 15:56:19 +0800

    > compile NEMU ysyx_22050606 张正 Linux zzGaruda 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  15:56:19 up  2:01,  7 users,  load average: 3.94, 3.65, 3.36

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -100,7 +100,7 @@ static int cmd_info(char *args)
 	if (strcmp(args, "r") == 0) {
 		isa_reg_display();
 	} else if (strcmp(args, "w") == 0) {
-		wp_disp();
+		printf("watch point info display uninplemente now\n");
 		return 0;
 	} else {
 		printf("Unavaliable args\n");
