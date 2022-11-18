796dfacac9ce5849969102f8b9fabd0c7f82c13b
tracer-ysyx2205 <tracer@ysyx.org>
2022-11-18 17:47:49 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  17:47:49 up  3:58,  7 users,  load average: 4.45, 3.88, 3.59

--- npc/logs/annotated/top.v
+++ npc/logs/annotated/top.v
@@ -200,6 +200,7 @@
 	// reg [23:0] vga_mem [524287:0];
 	reg [7:0] vga_mem [2099:0];
 	reg font_rom [49152:0];
+	wire [15:0] font_addr;
 	wire [7:0] word;
 	
 	initial begin
@@ -210,8 +211,9 @@
 	
 	// assign vga_data = vga_mem[{h_addr, v_addr}];
 	assign word = vga_mem[{font_h,font_v}]; // get the 8 bit ascii value
-	// assign vga_data = font_rom[{word,{h_addr%10'd70}[7:0]+{v_addr%9'd30}[7:0]}] ? 24'b1 : 24'b0;
-	assign vga_data = 24'b1;
+	assign font_addr = {word,{h_addr%10'd70}[7:0]+{v_addr%9'd30}[7:0]};
+	assign vga_data = font_rom[font_addr] ? 24'b111111111111111111111111 : 24'b000000000000000000000000;
+	// assign vga_data = 24'b111111111111111111111111;
 	
 	endmodule
 	
