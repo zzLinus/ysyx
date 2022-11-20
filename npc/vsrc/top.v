5193e8bf9428974c5d8f5bc77310d6e580a22858
tracer-ysyx2205 <tracer@ysyx.org>
2022-11-20 15:03:43 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  15:03:43 up  2:12,  4 users,  load average: 2.40, 2.27, 2.16

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -206,7 +206,7 @@ wire [3:0] font_cord_h;
 wire [11:0] font_data;
 
 initial begin
-    $readmemh("resource/vga_font2.txt", font_rom);
+    $readmemh("resource/vga_font.txt", font_rom);
     $readmemh("resource/test.txt", vga_mem);
     // $readmemh("resource/hhh2.txt", vga_mem);
 end
