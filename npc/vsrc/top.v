39b21c85c26cfed96936223e9bf60cefd6ef9d9f
tracer-ysyx2205 <tracer@ysyx.org>
2022-11-20 14:26:03 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  14:26:03 up  1:34,  4 users,  load average: 1.69, 2.06, 2.39

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -216,7 +216,7 @@ assign word = {{4'b0000},vga_mem[{font_v,font_h}]}; // get the 8 bit ascii value
 assign font_cord_v = {v_addr%9'd16}[3:0];
 assign font_cord_h = {h_addr%10'd9}[3:0];
 // assign font_addr = {word*12'd16+{8'b00000000,font_cord_v},font_cord_h}; // bit = word * row_num * bit_per_row + h_bit + v_bit * bit_per_row
-assign font_addr = word * 12'b16 * 12'b12 + font_cord_v * 12'b12 + font_cord_h; // bit = word * row_num * bit_per_row + h_bit + v_bit * bit_per_row
+assign font_addr = word * 12'd16 * 12'd12 + font_cord_v * 12'd12 + font_cord_h; // bit = word * row_num * bit_per_row + h_bit + v_bit * bit_per_row
 assign font_data = font_rom[font_addr];
 assign vga_data = font_data ? 24'b111111111111111111111111 : 24'b000000000000000000000000;
 
