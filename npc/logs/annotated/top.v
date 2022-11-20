8927f4f5b86d2417e93a32e8a633ad93c4f01cff
tracer-ysyx2205 <tracer@ysyx.org>
2022-11-20 17:26:49 +0800

    > sim RTL ysyx_22050606 张正 Linux zzGaruda 6.0.8-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 10 Nov 2022 21:14:22 +0000 x86_64 GNU/Linux  17:26:49 up  4:35,  4 users,  load average: 2.82, 2.62, 2.63

--- npc/logs/annotated/top.v
+++ npc/logs/annotated/top.v
@@ -221,7 +221,7 @@
 	always @(ascii_code) begin
 		if (ascii_code != 8'h00) begin
 			if(ascii_code == 8'ha9) begin
-				word_count += 70;
+				word_count += 100;
 				$display("word_count %d", word_count);
 			end
 			else
