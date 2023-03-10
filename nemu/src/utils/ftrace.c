99c3fb0aa0a53e2aa9d5e2fffed56efaadbf9ed3
Zheng Zhang <lz5333885@gmail.com>
2023-03-10 15:25:00 +0800

    init_ftrace finished

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -1,7 +1,23 @@
 #include <common.h>
 #include <elf.h>
 
-#define Elfw(type) Elf64_##type
+#define Elfw(type)        Elf64_##type
+#define ELF_FUNC_MAX      20
+#define ELF_FUNC_NAME_MAX 10
+
+typedef struct
+{
+    uint64_t addr;
+    char name[ELF_FUNC_NAME_MAX];
+} ElfFuncInfo;
+
+ElfFuncInfo elf_funcs[ELF_FUNC_MAX];
+
+uint32_t elf_nums = 0;
+
+void get_fname(FILE* file, char* dst, size_t offset, uint32_t start)
+{
+}
 
 void init_ftrace(const char* p)
 {
@@ -10,7 +26,7 @@ void init_ftrace(const char* p)
     Elfw(Shdr) sec_header;
     Elfw(Sym) symtab;
 
-    /**Elfw(Off) strtab_offset = 0;*/
+    Elfw(Off) strtab_offset = 0;
     Elfw(Off) symtab_offset = 0;
 
     uint64_t symtab_num;
@@ -41,7 +57,7 @@ void init_ftrace(const char* p)
                     sec_header.sh_name,
                     sec_header.sh_size,
                     sec_header.sh_offset);
-                /**strtab_offset = sec_header.sh_offset;*/
+                strtab_offset = sec_header.sh_offset;
                 break;
             }
             else if (sec_header.sh_type == SHT_SYMTAB)
@@ -56,12 +72,22 @@ void init_ftrace(const char* p)
             }
         }
 
-        fseek(file, symtab_offset, SEEK_SET);
-        for (int i = 0; i < symtab_num; ++i)
+        for (int i = 0; i < symtab_num; i++)
         {
+            fseek(file, symtab_offset + i * sizeof(symtab), SEEK_SET);
             fread(&symtab, sizeof(symtab), 1, file);
-						if(ELF64_ST_TYPE(symtab.st_info) == STT_FUNC)
-								printf("function symbol : %lx\n", symtab.st_value);
+            if (ELF64_ST_TYPE(symtab.st_info) == STT_FUNC)
+            {
+                printf("function symbol : value : %lx name : %d\n", symtab.st_value, symtab.st_name);
+                fseek(file, strtab_offset + symtab.st_name, SEEK_SET);
+                fgets(elf_funcs[elf_nums].name, ELF_FUNC_NAME_MAX, file);
+                elf_nums++;
+            }
+        }
+
+        for (int i = 0; i < elf_nums; i++)
+        {
+            printf("elf_funcs : addr : %lx name : %s\n", elf_funcs[i].addr, elf_funcs[i].name);
         }
 
         fclose(file);
