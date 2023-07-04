79ed06832ed6712a764606ed6909b497dd440d2f
Zheng Zhang <lz5333885@gmail.com>
2023-07-04 19:13:37 +0800

    pmem_read transplant

--- npc-riscv64/vsrc/DMEM.v
+++ npc-riscv64/vsrc/DMEM.v
@@ -1,32 +1,33 @@
-module DMEM(
+import "DPI-C" function void pmem_read(
+		input  longint raddr,
+		output longint rdata
+);
+
+import "DPI-C" function void pmem_write(
+		input longint waddr,
+		input longint wdata,
+		input byte wmask
+);
+
+module DMEM (
 		input mem_w_EN,
 		input mem_r_EN,
 		input [63:0] addr,
 		input [63:0] write_data,
-		output [63:0] mem_out
+		output reg [63:0] mem_out
 );
 
 // NOTE : e.g addr : 0x80008ff0 -> sd 64 bits
 //        --> memory[addr[15:0] >> 6] <= write_data
 //        --> memory[addr[15:6]] <= write_data
 
-reg [63:0] memory [575:0]; // stack top -> 0x80009000
-int i;
-
-initial begin
-		for(i=0; i<565; i=i+1) begin
-				memory[i] = 64'b0;
-		end
-end
-
 always @(*) begin
-		if(mem_w_EN)
-				memory[addr[15:6]] <= write_data;
-		//$display("\n** DMEM Module **");
-		//$display("addr    %x", mem_out);
-		//$display("mem_out %d", mem_out);
+		if(mem_w_EN) pmem_write(addr, write_data, 8'b1);
+		$display("\n** DMEM Module **");
+		$display("addr    %x", addr);
+		$display("mem_out %x", mem_out);
+		if(mem_r_EN) pmem_read(addr, mem_out);
+		else mem_out= 64'b0;
 end
-
-assign mem_out = mem_r_EN ? memory[addr[15:6]] : 64'b0;
 
 endmodule
